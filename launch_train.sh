6c1f266decdaba45defc5dbcd14e5fb57766aa59
Gabe <gabeorlanski@gmail.com>
2024-05-09 15:20:07 -0500

    Log

--- launch_train.sh
+++ launch_train.sh
@@ -6,4 +6,5 @@ shift 2
 mkdir -p logs
 LOG_FILE="${MODEL//\//\_}.log"
 LOG_FILE="logs/${LOG_FILE}"
+echo "Logging to ${LOG_FILE}"
 nohup bash python simple_train.py --model $MODEL --device $GPU $@ </dev/null > "${LOG_FILE}" 2>&1 &

7888e39cdd56f695035f478f0e1aebf78ef410b7
Gabe <gabeorlanski@gmail.com>
2024-05-09 17:45:52 -0500

    No XL

--- get_model_sizes.py
+++ get_model_sizes.py
@@ -6,12 +6,13 @@ for s in ["large", "base", "small", "xsmall"]:
 
 for s in ["14m", "70m", "160m", "410m", "1b", "1.4b"]:
     models.add(f"EleutherAI/pythia-{s}")
-for s in ["base", "small", "large", "small", "xl"]:
+for s in ["base", "small", "large", "small"]:
     models.add(f"google/t5-v1_1-{s}")
     models.add(f"google/mt5-{s}")
 
 out = {}
-for k in models:
+for k in sorted(models):
+    print(f"Checking {k}")
     model = AutoModelForTokenClassification.from_pretrained(k, num_labels=9)
     out[k.replace("/", "_")] = sum(
         p.numel() for p in model.parameters() if p.requires_grad

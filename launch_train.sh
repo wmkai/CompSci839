469f9fb7e00c184d55b2d5143d43f3e6af075f32
Gabe <gabeorlanski@gmail.com>
2024-05-09 15:46:29 -0500

    test

--- launch_train.sh
+++ launch_train.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 MODEL=$1
 GPU=$2
-shift 2
 
 mkdir -p logs
 LOG_FILE="${MODEL//\//\_}.log"
 LOG_FILE="logs/${LOG_FILE}"
 echo "Training ${MODEL} on GPU ${GPU}"
 echo "${LOG_FILE}"
-nohup bash train.sh </dev/null > "${LOG_FILE}" 2>&1 &
+nohup bash train.sh $@ </dev/null > "${LOG_FILE}" 2>&1 &
